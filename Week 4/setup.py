677ac6f1396dbf82e681ac2ae5fba4f65e56dd5e
Nicholas James <syncert@gmail.com>
2024-03-27 18:05:24 -0500

    Updated Project Organization, moved Project One to Week 4

---
+++ Week 4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+    name='MongoCRUD',
+    version='1.0',
+    description='Middleware that interacts with MongoDB for Applications CRUD functionality',
+    author='Nicholas Kreuziger',
+    author_email='nicholas.kreuziger@snhu.edu',
+    url='',
+    py_modules=['MongoCRUD'],
+)
